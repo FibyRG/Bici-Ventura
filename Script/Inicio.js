fdb6bf917783ce6486895541590cd83f7136df04
ItsDassiel <148930093+ItsDassiel@users.noreply.github.com>
2025-06-18 02:13:31 -0600

    (modifique el inicio,y cambie el login,pegue imagen etc)

--- Script/Inicio.js
+++ Script/Inicio.js
@@ -2,4 +2,20 @@ function mostrarFormulario() {
   const formulario = document.getElementById('formulario');
   formulario.style.display = 'block';
   formulario.scrollIntoView({ behavior: 'smooth' });
+
+
 }
+
+
+//Para dirigirse al dashboar
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.formulario__form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Evita el envío real del formulario
+    // Aquí podrías validar datos si lo necesitas
+    window.location.href = "../HTML/Dashboard.html";
+  });
+});
+
